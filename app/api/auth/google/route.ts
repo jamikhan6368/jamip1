6a6ef564ef6aea5c601cb205caad4f4862f7dfbd
jamikhan6368 <moinkhan.datori1239@gmail.com>
2025-11-24 14:07:24 -0800

    Create route.ts

---
+++ app/api/auth/google/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const redirect = searchParams.get("redirect_uri");
+
+  const googleUrl =
+    "https://accounts.google.com/o/oauth2/v2/auth?" +
+    new URLSearchParams({
+      client_id: process.env.GOOGLE_CLIENT_ID!,
+      redirect_uri: `${process.env.GOOGLE_REDIRECT_URI}`,
+      response_type: "code",
+      scope: "email profile",
+      access_type: "offline",
+      prompt: "consent"
+    }).toString();
+
+  return NextResponse.redirect(googleUrl);
+}
