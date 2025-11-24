c69fdf8069a1bb0a10c568bd72dc3f39f9fb4a35
jamikhan6368 <moinkhan.datori1239@gmail.com>
2025-11-24 13:49:20 -0800

    Create start.js

---
+++ auth/google/start.js
@@ -0,0 +1,19 @@
+// /api/auth/google/start
+import { google } from 'googleapis';
+
+export default async function handler(req, res) {
+  const redirect = req.query.redirect_uri;
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    redirect
+  );
+
+  const url = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    prompt: "consent",
+    scope: ["profile", "email"]
+  });
+
+  return res.redirect(url);
+}
