a323fd7e9602d78c1d62b72a58f7f85e6ab26b71
jamikhan6368 <moinkhan.datori1239@gmail.com>
2025-11-26 06:00:53 -0800

    Delete api directory

--- api/load.js
+++
@@ -1,7 +0,0 @@
-import automationCode from '../lib/automation.js';
-
-export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Content-Type', 'text/javascript');
-  res.status(200).send(automationCode);
-}
