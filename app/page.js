67847c8870b85ab5f5a17642d6a441a4d28944f6
jamikhan6368 <moinkhan.datori1239@gmail.com>
2025-11-26 08:28:05 -0800

    Delete app directory

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <main>
-      <h1>Runway Automation Backend</h1>
-      <p>API is running. Use /api/* endpoints from your extension.</p>
-    </main>
-  );
-}
